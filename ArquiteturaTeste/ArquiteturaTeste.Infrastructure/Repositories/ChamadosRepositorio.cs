6a601ba08d7b7b7c7595bc9912b1896cf8a66b08
Andre Alef <andrealef2008@hotmail.com>
2019-01-18 15:01:40 -0200

    Adicionando Arquivo ArquiteturaTeste com kanban

---
+++ ArquiteturaTeste/ArquiteturaTeste.Infrastructure/Repositories/ChamadosRepositorio.cs
@@ -0,0 +1,62 @@
+﻿using ArquiteturaTeste.Domain.Models;
+using ArquiteturaTeste.Domain.Services;
+using ArquiteturaTeste.Infrastructure.Data;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace ArquiteturaTeste.Infrastructure.Repositories
+{
+    public class ChamadosRepositorio : IChamadosService
+    {
+        private ContextChamadosTeste1 _context;
+        public ChamadosRepositorio()
+        {
+            _context = new ContextChamadosTeste1();
+        }
+
+        public void NovoChamado(NovoChamado chamado)
+        {
+            _context.chamados.Add(chamado);
+            _context.SaveChanges();
+        }
+
+        public void EditarStatus(NovoChamado chamado)
+        {
+            //    _context.Entry<NovoChamado>(chamado).State = System.Data.Entity.EntityState.Modified;
+            //  _context.SaveChanges();
+
+            var ChamadoAlterar = _context.chamados.First(x => x.Id == chamado.Id);
+            ChamadoAlterar.Assunto = chamado.Assunto;         
+            _context.SaveChanges();
+        }
+
+        public void CancelarChamado(NovoChamado chamado)
+        {
+            var ChamadoExcluir = _context.chamados.First(x => x.Id == chamado.Id);
+            _context.chamados.Remove(chamado);
+            _context.Set<NovoChamado>().Remove(ChamadoExcluir);
+            _context.SaveChanges();
+        }
+
+        public List<NovoChamado> ListarChamados()
+        {
+            return _context.chamados.ToList();
+        }
+
+        public NovoChamado ListarPorId(string id)
+        {
+
+            int idInt;
+            Int32.TryParse(id, out idInt);
+            return _context.chamados.First(x => x.Id == idInt);
+        
+        }
+
+        public void Dispose()
+        {
+            _context.Dispose();
+        }
+     
+    }
+}
---
+++ ArquiteturaTeste/ControleDeChamados/Controllers/ChamadosController.cs
@@ -0,0 +1,94 @@
+﻿using ArquiteturaTeste.Business.Services;
+using ArquiteturaTeste.Domain.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace ControleDeChamados.Controllers
+{
+    public class ChamadosController : Controller
+    {
+        private readonly ChamadosService appChamados;
+        public ChamadosController()
+        {
+            appChamados = ConstrutorChamados.ChamadosBusiness();
+        }
+
+        // GET: Chamados
+        public ActionResult Index()
+        {
+            var Listar = appChamados.ListarChamados();
+            return View(Listar);
+        }
+
+        public ActionResult Cadastrar()
+        {
+            return View();
+        }
+
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Cadastrar(NovoChamado chamado)
+        {
+            if (ModelState.IsValid)
+            {
+                appChamados.NovoChamado(chamado);
+                return RedirectToAction("Index");
+            }
+            return View(chamado);
+        }
+
+        public ActionResult Editar(string id)
+        {
+            var chamado = appChamados.ListarPorId(id);
+            if (chamado == null)
+                return HttpNotFound();
+
+            return View(chamado);
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Editar(NovoChamado chamado)
+        {
+            if (ModelState.IsValid)
+            {
+                appChamados.EditarStatus(chamado);
+                return RedirectToAction("Index");
+            }
+            return View(chamado);
+        }
+
+        public ActionResult Detalhes(string id)
+        {
+            var chamados = appChamados.ListarPorId(id);
+            if (chamados == null)
+                return HttpNotFound();
+
+            return View(chamados);
+        }
+
+        public ActionResult Excluir(string id)
+        {
+            var chamados = appChamados.ListarPorId(id);
+            if (chamados == null)
+                return HttpNotFound();
+
+            return View(chamados);
+        }
+
+        [HttpPost, ActionName("Excluir")]
+        [ValidateAntiForgeryToken]
+        public ActionResult ExcluirConfirmado(string id)
+        {
+            var chamados = appChamados.ListarPorId(id);
+            appChamados.CancelarChamado(chamados);
+            return RedirectToAction("Index");
+        }
+
+
+    }
+}
